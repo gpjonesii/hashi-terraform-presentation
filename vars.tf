0d48cad07c59f01029cce28a805db23640c2fe83
Greg Jones <gregory.jonesii@gmail.com>
2018-02-22 15:00:39 -0500

    initial commit

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "azure_location" {
+    description = "Azure Region all resources will be deployed to"
+    default = "US East 2"
+}
+
+variable "environment" {
+    description = "Deployment Environment for Tags"
+    default = "developement"
+}
